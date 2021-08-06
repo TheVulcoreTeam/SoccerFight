db97a7114891164497595bfec9f4ad2d43e618f7
Samu Garron <garron.garron@gmail.com>
2021-08-06 15:11:24 -0300

    deleting server

--- Autoloads/ClinetWS.gd
+++ Autoloads/ClinetWS.gd
@@ -1,19 +1,17 @@
 extends Node
 
 # The URL we will connect to
-export var websocket_url = "ws://echo.websocket.org"
+export var websocket_url = "ws://127.0.0.1:3000" 
+#export var websocket_url = "ws://echo.websocket.org" 
 
 # Our WebSocketClient instance
 var _client = WebSocketClient.new()
 
 func _ready():
 	# Connect base signals to get notified of connection open, close, and errors.
-	_client.connect("connection_closed", self, "_closed")
-	_client.connect("connection_error", self, "_closed")
+	_client.connect("connection_closed", self, "_closed1")
+	_client.connect("connection_error", self, "_closed2")
 	_client.connect("connection_established", self, "_connected")
-	# This signal is emitted when not using the Multiplayer API every time
-	# a full packet is received.
-	# Alternatively, you could check get_peer(1).get_available_packets() in a loop.
 	_client.connect("data_received", self, "_on_data")
 
 	# Initiate connection to the given URL.
@@ -21,11 +19,19 @@ func _ready():
 	if err != OK:
 		print("Unable to connect")
 		set_process(false)
-
-func _closed(was_clean = false):
+func _process(delta):
+	_client.poll()
+	
+func _closed1(was_clean = false):
 	# was_clean will tell you if the disconnection was correctly notified
 	# by the remote peer before closing the socket.
-	print("Closed, clean: ", was_clean)
+	print("Closed, clean: 1", was_clean)
+	set_process(false)
+		
+func _closed2(was_clean = false):
+	# was_clean will tell you if the disconnection was correctly notified
+	# by the remote peer before closing the socket.
+	print("Closed, clean: 2", was_clean)
 	set_process(false)
 
 func _connected(proto = ""):
@@ -57,7 +63,4 @@ func _on_data():
 #	print("Got data from server: ", yarn)
 	
 	
-func _process(delta):
-	# Call this in _process or _physics_process. Data transfer, and signals
-	# emission will only happen when calling this function.
-	_client.poll()
+

32bdc23aba9879df0c235e82397d27fc89c6bc16
Matías Valentín Muñoz Espinoza <writkas2@gmail.com>
2021-08-08 12:58:48 -0400

    Edges to soccer field

--- Game/Actors/Player/Player.gd
+++ Game/Actors/Player/Player.gd
@@ -1,30 +1,26 @@
 extends RigidBody2D
 
 
-const MAX_IMPULSE = 100
+const MAX_IMPULSE = 70
 
 var impulse = Vector2.ZERO
 
 
 func _physics_process(delta):
 	$Sprite.look_at(get_global_mouse_position())
-	
+
 
 func _integrate_forces(state):
-	print(impulse)
-	
 	if !impulse.is_equal_approx(Vector2.ZERO):
 		impulse.x = clamp(impulse.x, -MAX_IMPULSE, MAX_IMPULSE)
 		impulse.y = clamp(impulse.y, -MAX_IMPULSE, MAX_IMPULSE)
 		
 		state.apply_central_impulse(impulse)
 		impulse = Vector2.ZERO
-		print_debug(impulse)
 
 
 func mouse_impulse():
 	impulse = get_local_mouse_position()
-#	print_debug(impulse)
 
 
 func _input(event):
